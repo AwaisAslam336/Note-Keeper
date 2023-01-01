d9885c568f69ad0403f2bea71bb74b145c2ca6a6
Awais Aslam <awaisaslam336@gmail.com>
2023-01-01 22:52:39 +0500

    intial template

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Note from "./components/Note";
+
+function App(){
+return (
+    <body>
+        <Header />
+        <Note />
+        <Footer />
+    </body>
+);
+}
+
+export default App;
