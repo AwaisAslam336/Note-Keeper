3ead685bc4bb41c2a547f4307df22eaea7a3a091
Awais Aslam <awaisaslam336@gmail.com>
2023-01-05 20:34:42 +0500

    Add New Note

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(note) {
+    setNotes((preValue) => {
+      //return previous notes plus new note //added new note in notes
+      console.log([...preValue, note]);
+      return [...preValue, note];
+    });
+  }
   return (
-    <body>
+    <div>
       <Header />
-      <Note key={1} title="note title" content="note content" />
+      <CreateArea onAdd={addNote} />
+      {notes.map((note, index) => {
+        return <Note key={index} title={note.title} content={note.content} />;
+      })}
       <Footer />
-    </body>
+    </div>
   );
 }
 
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+
+  function holdNoteValue(event) {
+    //get name & value of target element
+    //it will be either title input or content textArea
+    const { name, value } = event.target;
+
+    setNote((preValue) => {
+      //return object with previous value but overwrite given element value
+      return { ...preValue, [name]: value };
+    });
+  }
   return (
     <div>
-      <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+      <form
+        onSubmit={(e) => {
+          //call parent onAdd function to save note in notes array
+          props.onAdd(note);
+          //after adding note in notes array; clear note field for new entry
+          setNote({
+            title: "",
+            content: "",
+          });
+          e.preventDefault();
+        }}
+      >
+        <input
+          onChange={holdNoteValue}
+          value={note.title}
+          name="title"
+          placeholder="Title"
+        />
+        <textarea
+          onChange={holdNoteValue}
+          value={note.content}
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+        />
+        <button type="submit">Add</button>
       </form>
     </div>
   );
