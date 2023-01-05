4f040f42309ee9e63e7e1d6f14835dbe5936e0c3
Awais Aslam <awaisaslam336@gmail.com>
2023-01-05 19:35:43 +0500

    added create Area & Delete Button Design

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,18 +2,15 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
 
-function App(){
-return (
+function App() {
+  return (
     <body>
-        <Header />
-        {notes.map((note)=>{
-            return (<Note key={note.key} title={note.title} content={note.content}/>);
-        })}
-        <Footer />
+      <Header />
+      <Note key={1} title="note title" content="note content" />
+      <Footer />
     </body>
-);
+  );
 }
 
 export default App;
